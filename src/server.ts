2a3f105c65f688e54db4f2e54a2b71e1d52c1fb4
walissonsou <walissonsouza96@hotmail.com>
2022-10-24 16:30:26 -0300

    Debugando a aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
-import express from 'express'; //
-import createCourse from './routes'
+import express from 'express'; 
 
 const app = express();
 
-app.get("/", createCourse);
+app.use(express.json());
 
 
-app.listen(3333);
+app.get("/", (req, res) => {
+  return res.json({message: "Oi papai"});
+})
+
+app.post("/courses", (req, res) => {  
+  const { name } = req.body;
+  return res.json({ name });
+})
+app.listen(3333, () => console.log("Server is running!"));
