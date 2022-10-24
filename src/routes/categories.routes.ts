404019b55386a2c11ff64a171e10d3031f65947d
walissonsou <walissonsouza96@hotmail.com>
2022-10-24 20:59:26 -0300

    Inserindo tipagem para categoria

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import { v4 as uuidV4} from 'uuid'
+import { ICategory } from '../model/category';
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: ICategory[] = [];
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  const category = { 
+  const category: ICategory = { 
     id: uuidV4(),
     name,
     description,
